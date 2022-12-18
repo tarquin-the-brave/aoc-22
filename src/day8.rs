d2a55b5722ee7167085231d933662c4054c50abd
tarquin-the-brave <tom.steavenson@gmail.com>
2022-12-18 21:06:25 +0000

    daay 8 part 1
    Signed-off-by: tarquin-the-brave <tom.steavenson@gmail.com>

--- src/day8.rs
+++ src/day8.rs
@@ -1,8 +1,106 @@
-#![allow(unused_variables)]
+use std::collections::HashSet;
 
-pub fn part1(input: String) -> u32 {
-    todo!()
+fn construct_grid(input: String) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect()
 }
+
+pub fn part1(input: String) -> usize {
+    let grid = construct_grid(input);
+    println!("{:?}", grid);
+
+    let height = grid.len();
+    let width = grid[0].len();
+
+    // Number of perimeter trees that are always visible
+    let perimeter_trees = 2 * height + 2 * width - 4;
+    println!("Perimeter trees: {perimeter_trees}");
+
+    let mut visible_trees = HashSet::<(usize, usize)>::new();
+    // Counting inner trees visible from the left
+    for j in 1..height - 1 {
+        let mut highest_tree = grid[j][0];
+        for i in 1..width - 1 {
+            let tree = grid[j][i];
+            println!(
+                "Highest: {}, current: {}, point: ({},{})",
+                highest_tree, tree, j, i
+            );
+            if tree > highest_tree {
+                println!(" Tree is taller and visible");
+                visible_trees.insert((j, i));
+                highest_tree = tree;
+            }
+        }
+    }
+    println!("{:?}", visible_trees);
+
+    // Counting inner trees visible from the top
+    for i in 1..width - 1 {
+        let mut highest_tree = grid[0][i];
+        for j in 1..height - 1 {
+            let tree = grid[j][i];
+            println!(
+                "Highest: {}, current: {}, point: ({},{})",
+                highest_tree, tree, j, i
+            );
+            if tree > highest_tree {
+                println!(" Tree is taller and visible");
+                visible_trees.insert((j, i));
+                highest_tree = tree;
+            }
+        }
+    }
+
+    println!("{:?}", visible_trees);
+
+    // Counting inner trees visible from the right
+    for j in 1..height - 1 {
+        let mut highest_tree = grid[j][width - 1];
+        for i in (1..width - 1).rev() {
+            let tree = grid[j][i];
+            println!(
+                "Highest: {}, current: {}, point: ({},{})",
+                highest_tree, tree, j, i
+            );
+            if tree > highest_tree {
+                println!(" Tree is taller and visible");
+                visible_trees.insert((j, i));
+                highest_tree = tree;
+            }
+        }
+    }
+    println!("{:?}", visible_trees);
+
+    // Counting inner trees visible from the bottom
+    for i in 1..width - 1 {
+        let mut highest_tree = grid[height - 1][i];
+        for j in (1..height - 1).rev() {
+            let tree = grid[j][i];
+            println!(
+                "Highest: {}, current: {}, point: ({},{})",
+                highest_tree, tree, j, i
+            );
+            if tree > highest_tree {
+                println!(" Tree is taller and visible");
+                visible_trees.insert((j, i));
+                highest_tree = tree;
+            }
+        }
+    }
+
+    println!("{:?}", visible_trees);
+
+    visible_trees.len() + perimeter_trees
+}
+
+#[allow(unused_variables)]
 pub fn part2(input: String) -> u32 {
     todo!()
 }
