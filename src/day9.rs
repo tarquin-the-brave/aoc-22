35452a08e2da86e5ca4408118f2905eee89a96e6
tarquin-the-brave <tom.steavenson@gmail.com>
2022-12-19 19:39:18 +0000

    day9 part 2 - not working yet
    Signed-off-by: tarquin-the-brave <tom.steavenson@gmail.com>

--- src/day9.rs
+++ src/day9.rs
@@ -75,7 +75,107 @@ pub fn part1(input: String) -> usize {
     game.tail_visited_count()
 }
 
-#[allow(unused_variables)]
-pub fn part2(input: String) -> u32 {
-    todo!()
+/// Part 1 implemented a GameState using an approximation
+/// of the game physics that worked for a two knot rope,
+/// this is a fuller treatment of the game physics as
+/// described in the problem description.
+struct GameState2 {
+    rope: Vec<(i32, i32)>,
+    tail_visited: HashSet<(i32, i32)>,
+}
+
+impl GameState2 {
+    fn new(length: u32) -> Self {
+        let start = (0, 0);
+        let mut rope = Vec::new();
+        for _ in 0..length {
+            rope.push(start.clone());
+        }
+        let mut tail_visited = HashSet::new();
+        tail_visited.insert(start.clone());
+        Self { rope, tail_visited }
+    }
+
+    fn move_head(&mut self, direction: &Direction) {
+        use Direction::*;
+
+        let mut knots = self.rope.iter_mut();
+        let head = knots.next().unwrap();
+        match direction {
+            Up => *head = (head.0, head.1 + 1),
+            Down => *head = (head.0, head.1 - 1),
+            Left => *head = (head.0 - 1, head.1),
+            Right => *head = (head.0 + 1, head.1),
+        }
+
+        let mut last_knot_moved_to = head.clone();
+
+        for knot in knots {
+            if !Self::move_knot(&last_knot_moved_to, knot) {
+                break;
+            }
+            last_knot_moved_to = knot.clone();
+        }
+    }
+
+    // move the knot if it needs to catch up with the previous
+    // knot.  If the knot moves, returns true.
+    fn move_knot(previous: &(i32, i32), knot: &mut (i32, i32)) -> bool {
+        match (previous.0 - knot.0, previous.1 - knot.1) {
+            // Above
+            (0, 2) => knot.1 += 1,
+            // Above-right / Right-above
+            (1, 2) | (2, 1) | (2, 2) => {
+                knot.0 += 1;
+                knot.1 += 1
+            }
+            // Right
+            (2, 0) => knot.0 += 1,
+            // Right-down / Down-right
+            (2, -1) | (1, -2) | (2, -2) => {
+                knot.0 += 1;
+                knot.1 -= 1
+            }
+            // Down
+            (0, -2) => knot.1 -= 1,
+            // Down-left / Left-down
+            (-1, -2) | (-2, -1) | (-2, -2) => {
+                knot.0 -= 1;
+                knot.1 -= 1
+            }
+            // Left
+            (-2, 0) => knot.0 -= 1,
+            // Left-up / Up-left
+            (-2, 1) | (-1, 2) | (-2, 2) => {
+                knot.0 -= 1;
+                knot.1 += 1
+            }
+            (x, y) => {
+                if x.abs() > 1 || y.abs() > 1 {
+                    panic!("Rope broke! x: {x}, y: {y}");
+                }
+                return false;
+            }
+        };
+        true
+    }
+
+    fn tail_visited_count(&self) -> usize {
+        self.tail_visited.len()
+    }
+}
+
+pub fn part2(input: String) -> usize {
+    use itertools::Itertools as _;
+    let mut game = GameState2::new(10);
+    for line in input.lines() {
+        let (direction_unparsed, times_unparsed) = line.split(' ').collect_tuple().unwrap();
+        let direction = Direction::parse(direction_unparsed);
+        let times: u32 = times_unparsed.parse().unwrap();
+
+        for _ in 0..times {
+            game.move_head(&direction);
+        }
+    }
+    game.tail_visited_count()
 }
