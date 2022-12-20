cac69f06770493515a074133f24b8d7191e070be
tarquin-the-brave <tomsteavenson@gmail.com>
2022-12-20 07:08:28 +0000

    day11 part1
    Signed-off-by: tarquin-the-brave <tomsteavenson@gmail.com>

--- src/day11.rs
+++ src/day11.rs
@@ -14,6 +14,7 @@ struct Monkey {
     test_divisible_by: u32,
     to_true: usize,
     to_false: usize,
+    items_count: u32,
 }
 
 impl Monkey {
@@ -46,17 +47,40 @@ impl Monkey {
             .unwrap();
         let _to_false = input[5].trim().split(' ').collect::<Vec<_>>()[5];
         let to_false = _to_false.parse().unwrap();
+
         Self {
             items,
             op,
             test_divisible_by,
             to_true,
             to_false,
+            items_count: 0,
         }
+    }
+
+    fn run_turn(&mut self) -> Vec<(usize, u32)> {
+        let mut pass_to_monkeys = Vec::<(usize, u32)>::new();
+
+        while let Some(worry) = self.items.pop_front() {
+            let worry = match self.op {
+                Op::Plus(x) => worry + x,
+                Op::Multiply(x) => worry * x,
+                Op::Square => worry * worry,
+            } / 3;
+            let to_monkey: usize = if worry % self.test_divisible_by == 0 {
+                self.to_true
+            } else {
+                self.to_false
+            };
+            pass_to_monkeys.push((to_monkey, worry));
+            self.items_count += 1;
+        }
+
+        pass_to_monkeys
     }
 }
 
-pub fn part1(input: String) -> usize {
+pub fn part1(input: String) -> u32 {
     use itertools::Itertools as _;
     let mut monkeys = input
         .lines()
@@ -65,7 +89,25 @@ pub fn part1(input: String) -> usize {
         .map(|chunk| Monkey::parse(chunk.map(|s| s.to_string()).collect()))
         .collect::<Vec<Monkey>>();
     println!("{:?}", monkeys);
-    6
+
+    for round in 0..20 {
+        for idx in 0..monkeys.len() {
+            let updates = monkeys[idx].run_turn();
+            for (idx, worry) in updates {
+                monkeys[idx].items.push_back(worry);
+            }
+        }
+        println!("Round: {}", round + 1);
+        println!("{:?}", monkeys.iter().map(|m| &m.items).collect::<Vec<_>>());
+    }
+
+    let mut scores = monkeys
+        .iter()
+        .map(|monkey| monkey.items_count)
+        .sorted()
+        .collect::<Vec<_>>();
+
+    scores.pop().unwrap() * scores.pop().unwrap()
 }
 
 #[allow(unused_variables)]
